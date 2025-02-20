174db78d63322d982f5e0aca2b7705d9a7be1a10
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-02-20 11:38:03 +0100

    Fixed bug that cased to use previous shrinked datasets for next experiment series

--- compute_cf/experiment_series.py
+++ compute_cf/experiment_series.py
@@ -14,7 +14,6 @@ def evaluate_cf_quality(Xs, ys, Xs_test, ys_test, vae_path, surrogate_path, N_te
 
     # Try to re-create original W1-values and compare CF to original data point
     cfs = []
-    targets = []
     for idx, (target_cf, target_value) in enumerate(zip(Xs_test, ys_test)):
         print(f"\n\nCurrently computing CF {idx} of {Xs_test.shape[0] - 1}")
         print(f"Target CF: {target_cf.tolist()}")
@@ -29,8 +28,7 @@ def evaluate_cf_quality(Xs, ys, Xs_test, ys_test, vae_path, surrogate_path, N_te
             eta=0.01
         )
         cfs.append(config_cf)
-        targets.append(target_cf)
-    return np.array(cfs), np.array(targets)
+    return np.array(cfs)
 
 
 def remove_k_nearest_neighbors(Xs, ys, sample, k):
@@ -40,14 +38,13 @@ def remove_k_nearest_neighbors(Xs, ys, sample, k):
 
 
 def train_on_partial_data(path, logging_dir, total_repetitions=5):
-    (Xs, ys), (Xs_val, ys_val), (Xs_test, ys_test) = load_data(path, splitted=True)
-
-    # Randomly pick a data point in Xs
-    selected_sample = Xs[np.random.randint(low=0, high=Xs.shape[0])]
     for repetition in range(total_repetitions):
         ks = [1_000, 2_500, 5_000, 7_500, 10_000, 15_000, 20_000, 25_000, 50_000, 100_000]
         for k in ks:
-            # - Remove k nearest neighbors
+            # Remove k nearest neighbors
+            (Xs, ys), (Xs_val, ys_val), (Xs_test, ys_test) = load_data(path, splitted=True)
+            # Randomly pick a data point in Xs
+            selected_sample = Xs[np.random.randint(low=0, high=Xs.shape[0])]
             Xs, ys = remove_k_nearest_neighbors(Xs, ys, selected_sample, k)
 
             # - Train new VAE
@@ -69,7 +66,9 @@ def train_on_partial_data(path, logging_dir, total_repetitions=5):
             # - Check quality of counterfactuals
             cfs_file = f"{vae_path}/rep_{repetition}_cfs_{k}_removed.npy"
             targets_file = f"{vae_path}/rep_{repetition}_targets_{k}_removed.npy"
+            y_targets_file = f"{vae_path}/rep_{repetition}_y_targets_{k}_removed.npy"
             if not os.path.isfile(cfs_file):
                 cfs, targets = evaluate_cf_quality(Xs, ys, Xs_test, ys_test, vae_path, surrogate_path)
                 np.save(cfs_file, cfs)
-                np.save(targets_file, targets)
+                np.save(targets_file, Xs_test)
+                np.save(y_targets_file, ys_test)
