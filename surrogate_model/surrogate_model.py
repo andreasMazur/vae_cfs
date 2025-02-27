e3a67cb5284d7bd38f4a2a1ed12b4fc779c279e1
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-02-27 10:05:52 +0100

    Changed training verbosity

--- surrogate_model/surrogate_model.py
+++ surrogate_model/surrogate_model.py
@@ -77,8 +77,7 @@ def train_surrogate(Xs, ys, Xs_val, ys_val, Xs_test, ys_test, dimensions=None, l
         batch_size=64,
         epochs=10_000,
         validation_data=(Xs_val, ys_val),
-        callbacks=[cp_callback, csv_callback, stop_callback, tensorboard_cb],
-        verbose=2
+        callbacks=[cp_callback, csv_callback, stop_callback, tensorboard_cb]
     )
     regressor.save(f"{logging_dir}/trained_surrogate.keras")
     evaluate(regressor, (Xs_test, ys_test))
