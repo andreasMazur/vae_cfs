df9178ac98ff8de7446fcebbc6cc89b3ecfd886a
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-03-07 18:51:02 +0100

    Refactored multiprocessing

--- compute_cf/experiment_series.py
+++ compute_cf/experiment_series.py
@@ -49,54 +49,58 @@ def remove_k_nearest_neighbors(Xs, ys, sample, k):
     return Xs[keep], ys[keep], Xs[remove], ys[remove]
 
 
-def multiprocessing_experiment_series(data_path, logging_dir, repetitions):
-    logging_dirs = [f"{logging_dir}/repetition_{i}" for i in range(repetitions)]
-    with Pool(repetitions) as p:
-        p.starmap(train_on_partial_data, [(data_path, ld) for ld in logging_dirs])
-
-
-def train_on_partial_data(data_path, logging_dir, n_test=200):
+def train_on_partial_data_wrapper(data_path, logging_dir, repetitions=100, n_test=200, processes=10):
     os.makedirs(logging_dir, exist_ok=True)
 
     # Select test data
     _, _, (Xs_test, ys_test) = load_data(data_path, splitted=True)
     Xs_test, ys_test = Xs_test[:n_test], ys_test[:n_test]
 
-    for k in [4681 * i for i in range(0, 20)][::-1]:
-        # Randomly pick a data point in test set and remove its k nearest neighbors in the training data
-        (Xs, ys), (Xs_val, ys_val), _ = load_data(data_path, splitted=True)
-        selected_outcome = ys_test[np.random.randint(low=0, high=n_test)]
-        Xs, ys, Xs_removed, ys_removed = remove_k_nearest_neighbors(Xs, ys, selected_outcome, k)
-
-        # Train new VAE
-        vae_path = f"{logging_dir}/vae_{k}_nn_removed"
-        if not os.path.isdir(vae_path):
-            training_history = None
-            while training_history is None or np.isnan(training_history.history["loss"][-1]):
-                training_history = train_vae(Xs, Xs_val, Xs_test, logging_dir=vae_path)
-            np.save(f"{vae_path}/Xs_train.npy", Xs)
-            np.save(f"{vae_path}/ys_train.npy", ys)
-
-        # Train new surrogate model
-        surrogate_path = f"{logging_dir}/surrogate_{k}_nn_removed"
-        if not os.path.isdir(surrogate_path):
-            train_surrogate(
-                Xs, ys, Xs_val, ys_val, Xs_test, ys_test, dimensions=[32, 32], logging_dir=surrogate_path
+    # Run experiments
+    for rep in range(repetitions):
+        logging_dir = f"{logging_dir}/repetition_{rep}"
+        with Pool(processes) as p:
+            p.starmap(
+                train_on_partial_data,
+                [(k, logging_dir, data_path, Xs_test, ys_test) for k in [4681 * i for i in range(0, 20)][::-1]]
             )
 
-        # Compute counterfactuals
-        cfs_file = f"{vae_path}/cfs.npy"
-        cf_preds_file = f"{vae_path}/cf_preds.npy"
-        targets_file = f"{vae_path}/targets.npy"
-        y_targets_file = f"{vae_path}/y_targets.npy"
-        if not os.path.isfile(cfs_file):
-            cfs, cf_preds = compute_cf_wrapper(Xs, ys, Xs_test, ys_test, vae_path, surrogate_path)
-            np.save(cfs_file, cfs)
-            np.save(cf_preds_file, cf_preds)
-            np.save(targets_file, Xs_test)
-            np.save(y_targets_file, ys_test)
 
-        # Train classifier
-        classifier_path = f"{logging_dir}/classifier_{k}_nn_removed"
-        if not os.path.isfile(cfs_file):
-            train_clf(Xs, Xs_removed, classifier_path)
+def train_on_partial_data(k, logging_dir, data_path, Xs_test, ys_test):
+    # Randomly pick a data point in test set and remove its k nearest neighbors in the training data
+    (Xs, ys), (Xs_val, ys_val), _ = load_data(data_path, splitted=True)
+    selected_outcome = ys_test[np.random.randint(low=0, high=ys_test.shape[0])]
+    Xs, ys, Xs_removed, ys_removed = remove_k_nearest_neighbors(Xs, ys, selected_outcome, k)
+
+    # Train new VAE
+    vae_path = f"{logging_dir}/vae_{k}_nn_removed"
+    if not os.path.isdir(vae_path):
+        training_history = None
+        while training_history is None or np.isnan(training_history.history["loss"][-1]):
+            training_history = train_vae(Xs, Xs_val, Xs_test, logging_dir=vae_path)
+        np.save(f"{vae_path}/Xs_train.npy", Xs)
+        np.save(f"{vae_path}/ys_train.npy", ys)
+
+    # Train new surrogate model
+    surrogate_path = f"{logging_dir}/surrogate_{k}_nn_removed"
+    if not os.path.isdir(surrogate_path):
+        train_surrogate(
+            Xs, ys, Xs_val, ys_val, Xs_test, ys_test, dimensions=[32, 32], logging_dir=surrogate_path
+        )
+
+    # Compute counterfactuals
+    cfs_file = f"{vae_path}/cfs.npy"
+    cf_preds_file = f"{vae_path}/cf_preds.npy"
+    targets_file = f"{vae_path}/targets.npy"
+    y_targets_file = f"{vae_path}/y_targets.npy"
+    if not os.path.isfile(cfs_file):
+        cfs, cf_preds = compute_cf_wrapper(Xs, ys, Xs_test, ys_test, vae_path, surrogate_path)
+        np.save(cfs_file, cfs)
+        np.save(cf_preds_file, cf_preds)
+        np.save(targets_file, Xs_test)
+        np.save(y_targets_file, ys_test)
+
+    # Train classifier
+    classifier_path = f"{logging_dir}/classifier_{k}_nn_removed"
+    if not os.path.isfile(cfs_file):
+        train_clf(Xs, Xs_removed, classifier_path)
