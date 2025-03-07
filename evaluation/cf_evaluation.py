44969fd5be9e9afc0b772a82a8bc067e57fdd9fb
Andreas Mazur <amazur@techfak.uni-bielefeld.de>
2025-03-07 18:40:04 +0100

    Updated evaluation functions

--- evaluation/cf_evaluation.py
+++ evaluation/cf_evaluation.py
@@ -1,3 +1,5 @@
+from data.data_loading import load_data
+
 from matplotlib import pyplot as plt
 
 import os
@@ -12,34 +14,81 @@ FEATURE_NAMES = {
 N_TRAINING_SAMPLES = 93_627
 
 
-def cf_evaluation(logs_dir, plot=True, max_removed=20):
+def get_most_similar_config(Xs, ys, x_cf):
+    closest_feature_indices = np.linalg.norm(x_cf[:, None, :] - Xs[None, :, :], axis=-1).argmin(axis=-1)
+    return Xs[closest_feature_indices], ys[closest_feature_indices]
+
+
+def cf_evaluation(data_path, logs_dir, plot=True, max_removed=20):
+    Xs_all, _ = load_data(data_path, splitted=False)
+    (Xs, ys), _, _ = load_data(data_path, splitted=True)
     for feature in range(4):
-        plot_values = []
-        for repetition in [d for d in os.listdir(logs_dir) if "repetition_" in d][:1]:
-            repetition_plot_values = []
+        gt_plot_values = []
+        closest_plot_values = []
+        data_misses = False
+        for repetition in [d for d in os.listdir(logs_dir) if "repetition_" in d]:
+            repetition_gt_plot_values = []
+            repetition_closest_plot_values = []
             for removed in [4681 * i for i in range(0, 20)][::-1][:max_removed]:
-                # Calculate mean absolute relative difference
-                cfs = np.load(f"{logs_dir}/{repetition}/vae_{removed}_nn_removed/cfs.npy")[:, feature]
-                targets = np.load(
-                    f"{logs_dir}/{repetition}/vae_{removed}_nn_removed/targets.npy"
-                )[:, feature]
-                total_feature_difference = np.abs(cfs - targets).mean()
-                repetition_plot_values.append((N_TRAINING_SAMPLES - removed, total_feature_difference))
-            plot_values.append(repetition_plot_values)
+                ########################################################################################
+                # Calculate mean absolute relative difference (w.r.t. true process config for target y)
+                ########################################################################################
+                try:
+                    cfs = np.load(f"{logs_dir}/{repetition}/vae_{removed}_nn_removed/cfs.npy")
+                    cfs_feature = cfs[:, feature]
+                    true_x_for_target = np.load(
+                        f"{logs_dir}/{repetition}/vae_{removed}_nn_removed/targets.npy"
+                    )[:, feature]
+                    total_feature_difference = np.abs(cfs_feature - true_x_for_target).mean()
+                    repetition_gt_plot_values.append(
+                        ((N_TRAINING_SAMPLES - removed) * 100 / Xs_all.shape[0], total_feature_difference)
+                    )
+                except FileNotFoundError:
+                    print(f"Data misses in: {logs_dir}/{repetition}/vae_{removed}_nn_removed/")
+                    data_misses = True
+                    break
+
+                #############################################################################################
+                # Calculate mean absolute relative difference (w.r.t. most similar config to counterfactual)
+                #############################################################################################
+                closest_x, _ = get_most_similar_config(Xs, ys, cfs)
+                closest_x_feature = closest_x[:, feature]
+                total_feature_difference = np.abs(cfs_feature - closest_x_feature).mean()
+                repetition_closest_plot_values.append(
+                    ((N_TRAINING_SAMPLES - removed) * 100 / Xs_all.shape[0], total_feature_difference)
+                )
+
+            gt_plot_values.append(repetition_gt_plot_values)
+            closest_plot_values.append(repetition_closest_plot_values)
+        # Don't add repetitions which lack data
+        if data_misses:
+            continue
 
         # Compute mean over repetitions
-        plot_values = np.array(plot_values)
-        plot_values[:, :, 1] = plot_values[:, :, 1] / (targets.max() - targets.min())
+        gt_plot_values = np.array(gt_plot_values)
+        gt_plot_values[:, :, 1] = gt_plot_values[:, :, 1] / (true_x_for_target.max() - true_x_for_target.min())
+        closest_plot_values = np.array(closest_plot_values)
+        closest_plot_values[:, :, 1] = closest_plot_values[:, :, 1] / (Xs[:, feature].max() - Xs[:, feature].min())
 
         # Save mean-values and std-values
-        plot_means = plot_values.mean(axis=0)
-        plt_std = plot_values.std(axis=0)[..., 1]
-        np.savetxt(f"{logs_dir}/mean_values_{FEATURE_NAMES[feature]}.csv", plot_means, delimiter=",", fmt="%f")
-        np.savetxt(f"{logs_dir}/std_values_{FEATURE_NAMES[feature]}.csv", plt_std, delimiter=",", fmt="%f")
+        test_plot_means = gt_plot_values.mean(axis=0)
+        test_plt_std = gt_plot_values.std(axis=0)[..., 1]
+        np.savetxt(f"{logs_dir}/test_mean_values_{FEATURE_NAMES[feature]}.csv", test_plot_means, delimiter=",", fmt="%f")
+        np.savetxt(f"{logs_dir}/test_std_values_{FEATURE_NAMES[feature]}.csv", test_plt_std, delimiter=",", fmt="%f")
+
+        closest_plot_means = closest_plot_values.mean(axis=0)
+        closest_plt_std = closest_plot_values.std(axis=0)[..., 1]
+        np.savetxt(f"{logs_dir}/closest_mean_values_{FEATURE_NAMES[feature]}.csv", closest_plot_means, delimiter=",", fmt="%f")
+        np.savetxt(f"{logs_dir}/closest_std_values_{FEATURE_NAMES[feature]}.csv", closest_plt_std, delimiter=",", fmt="%f")
 
         if plot:
-            plt.plot(plot_means[:, 0], plot_means[:, 1], color="blue")
-            plt.errorbar(plot_means[:, 0], plot_means[:, 1], fmt="none", capsize=5, yerr=plt_std, color="blue")
+            plt.plot(test_plot_means[:, 0], test_plot_means[:, 1], color="blue", label="test")
+            plt.errorbar(test_plot_means[:, 0], test_plot_means[:, 1], fmt="none", capsize=5, yerr=test_plt_std, color="blue")
+
+            plt.plot(closest_plot_means[:, 0], closest_plot_means[:, 1], color="red", label="closest")
+            plt.errorbar(closest_plot_means[:, 0], closest_plot_means[:, 1], fmt="none", capsize=5, yerr=closest_plt_std, color="red")
+
             plt.title(f"Mean over absolute errors: {FEATURE_NAMES[feature]}")
             plt.grid()
+            plt.legend()
             plt.show()
--- evaluation/table_look_up.py
+++ evaluation/table_look_up.py
@@ -1,9 +1,9 @@
+from data.data_loading import load_data
+from evaluation.cf_evaluation import N_TRAINING_SAMPLES, get_most_similar_config
 from matplotlib import pyplot as plt
 
 import numpy as np
 import os
-
-from evaluation.cf_evaluation import N_TRAINING_SAMPLES
 
 
 def table_look_up(configurations, angle_outcomes, target_angle):
@@ -11,12 +11,14 @@ def table_look_up(configurations, angle_outcomes, target_angle):
     return configurations[closest_outcome_idx], angle_outcomes[closest_outcome_idx]
 
 
-def table_look_up_experiment(experiment_path, plot=True, max_removed=20):
+def table_look_up_experiment(data_path, experiment_path, plot=True, max_removed=20):
+    Xs_all, ys_all = load_data(data_path, splitted=False)
+
     # Average plot-values over repetitions
-    average_table_plot_values, average_cf_plot_values = [], []
+    average_table_plot_values, average_cf_plot_values, average_closest_plot_values = [], [], []
     for repetition in [d for d in os.listdir(experiment_path) if "repetition_" in d]:
         # Plot absolute difference vs. k
-        table_plot_values, cf_plot_values = [], []
+        table_plot_values, cf_plot_values, closest_plot_values = [], [], []
         data_misses = False
         for k in [4681 * i for i in range(0, 20)][::-1][:max_removed]:
             folder_name = f"{experiment_path}/{repetition}/vae_{k}_nn_removed"
@@ -27,6 +29,7 @@ def table_look_up_experiment(experiment_path, plot=True, max_removed=20):
                 ys = np.load(f"{folder_name}/ys_train.npy")
                 Xs_test = np.load(f"{folder_name}/targets.npy")
                 ys_test = np.load(f"{folder_name}/y_targets.npy")
+                cfs = np.load(f"{folder_name}/cfs.npy")
                 cf_preds = np.load(f"{folder_name}/cf_preds.npy")
             except FileNotFoundError:
                 print(f"Data misses in: {folder_name}")
@@ -41,13 +44,10 @@ def table_look_up_experiment(experiment_path, plot=True, max_removed=20):
             for target_conf, target_outcome in zip(Xs_test, ys_test):
                 # Look up closest configuration in training data
                 conf, outcome = table_look_up(Xs, ys, target_angle=target_outcome)
-
                 # Calculate absolute difference
                 absolute_difference = np.abs(outcome - target_outcome)
                 absolute_differences.append(absolute_difference)
-
-            # Remember plot-values
-            table_plot_values.append((N_TRAINING_SAMPLES - k, np.mean(absolute_differences)))
+            table_plot_values.append(((N_TRAINING_SAMPLES - k) * 100 / Xs_all.shape[0], np.mean(absolute_differences)))
 
             #######################################################
             # Calculate absolute differences for table CF approach
@@ -57,7 +57,22 @@ def table_look_up_experiment(experiment_path, plot=True, max_removed=20):
                 # Calculate absolute difference
                 absolute_difference = np.abs(cf_outcome - target_outcome)
                 absolute_differences.append(absolute_difference)
-            cf_plot_values.append((N_TRAINING_SAMPLES - k, np.mean(absolute_differences)))
+            cf_plot_values.append(
+                ((N_TRAINING_SAMPLES - k) * 100 / Xs_all.shape[0], np.mean(absolute_differences))
+            )
+
+            #####################################################################################
+            # Calculate absolute differences between CF-predictions and closest-data predictions
+            #####################################################################################
+            _, closest_x_y = get_most_similar_config(Xs_all, ys_all, cfs)
+            absolute_differences = []
+            for cxy, cf_outcome in zip(closest_x_y, cf_preds):
+                # Calculate absolute difference
+                absolute_difference = np.abs(cf_outcome - cxy)
+                absolute_differences.append(absolute_difference)
+            closest_plot_values.append(
+                ((N_TRAINING_SAMPLES - k) * 100 / Xs_all.shape[0], np.mean(absolute_differences))
+            )
 
         # Don't add repetitions which lack data
         if data_misses:
@@ -66,6 +81,7 @@ def table_look_up_experiment(experiment_path, plot=True, max_removed=20):
         # Remember repetition plot-values
         average_table_plot_values.append(table_plot_values)
         average_cf_plot_values.append(cf_plot_values)
+        average_closest_plot_values.append(closest_plot_values)
 
     # Denominator for normalization
     max_min_distance = ys_test.max() - ys_test.min()
@@ -94,6 +110,18 @@ def table_look_up_experiment(experiment_path, plot=True, max_removed=20):
         fmt="%f"
     )
 
+    # Average over closest repetitions
+    average_closest_plot_values = np.array(average_closest_plot_values)
+    average_closest_plot_values[..., 1] = average_closest_plot_values[..., 1] / (ys_all.max() - ys_all.min())
+    closest_plot_values_stds = average_closest_plot_values.std(axis=0)[:, 1]
+    average_closest_plot_values = average_closest_plot_values.mean(axis=0)
+    np.savetxt(
+        f"{experiment_path}/closest_abs_pred_dev.csv",
+        np.concatenate([average_closest_plot_values, closest_plot_values_stds[:, None]], axis=-1),
+        delimiter=",",
+        fmt="%f"
+    )
+
     if plot:
         # Table graph
         plt.plot(
@@ -118,6 +146,18 @@ def table_look_up_experiment(experiment_path, plot=True, max_removed=20):
             capsize=5,
             yerr=cf_plot_values_stds,
             color="red"
+        )
+        # Closest graph
+        plt.plot(
+            average_closest_plot_values[:, 0], average_closest_plot_values[:, 1], color="green", label="Closest"
+        )
+        plt.errorbar(
+            average_closest_plot_values[:, 0],
+            average_closest_plot_values[:, 1],
+            fmt="none",
+            capsize=5,
+            yerr=closest_plot_values_stds,
+            color="green"
         )
 
         plt.title("Table look up vs. Counterfactuals")
